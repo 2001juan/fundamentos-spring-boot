5d9f740c9008c291ef0d2ae0fc931ac2f40b8303
Juan Ramírez <juanchoramirez@gmail.com>
2022-06-01 12:03:30 -0500

    Se incluyen beans y configuraciones

---
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/bean/MyBeanImplement.java
@@ -0,0 +1,9 @@
+package com.fundamentosplatzi.springboot.fundamentos.bean;
+
+public class MyBeanImplement implements MyBean{
+
+    @Override
+    public void print() {
+        System.out.println("Tome desde mi implementación propia del bean");
+    }
+}
