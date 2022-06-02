9bbb80a3e111b5d78deba8e4b854151e38690ef4
Juan Ramírez <juanchoramirez@gmail.com>
2022-06-02 15:26:58 -0500

    se crea el controller y se carga pagina web con parametros port y path

--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class FundamentosApplication implements CommandLineRunner {
